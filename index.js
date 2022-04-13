c730a640ee17dff1a78978683ecc05ca416a7f5a
Josué Lobo <darksiders20005@gmail.com>
2022-04-13 19:29:52 -0300

    refactor: update url to real target

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const puppeteer = require('puppeteer');
+const readline = require('readline-sync');
 
-async function main() {
+async function inputUser() {
+  const question = readline.question('Qual item quer procurar? ');
+  main(question)
+}
+
+async function main(item) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://roymusthang.github.io');
+  await page.goto(`https://www.mercadolivre.com.br/${item}`);
   await page.screenshot({ path: 'ma.png' });
   await browser.close();
 }
 
-main()
+inputUser()
