8273b635d862f319a3b565dbe4f4cb40ce6ace5a
Josué Lobo <darksiders20005@gmail.com>
2022-04-14 00:31:46 -0300

    refactor: url in variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const readline = require('readline-sync');
 
+const site = 'https://www.mercadolivre.com.br';
+
 async function inputUser() {
   const question = readline.question('Qual item quer procurar? ');
   main(question)
@@ -9,7 +11,7 @@ async function inputUser() {
 async function main(item) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(`https://www.mercadolivre.com.br/${item}`);
+  await page.goto(site);
   await browser.close();
 }
 
